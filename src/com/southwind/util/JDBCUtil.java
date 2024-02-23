0ae2bea2007bf1c35326910b04741fd38b9c0558
snowynight-sun <77873783+snowynight-sun@users.noreply.github.com>
2024-02-23 14:33:45 +0800

    Add files via upload

---
+++ src/com/southwind/util/JDBCUtil.java
@@ -0,0 +1,47 @@
+package com.southwind.util;
+
+import java.sql.*;
+
+public class JDBCUtil {
+    private static String driverName = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/dormitory?serverTimezone=UTC";
+    private static String user = "root";
+    private static String password = "mmnhsnmt";
+
+    static {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void release(Connection connection, Statement statement, ResultSet resultSet){
+        try {
+            if(connection != null){
+                connection.close();
+            }
+            if(statement != null){
+                statement.close();
+            }
+            if(resultSet != null){
+                resultSet.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+   public static void main(String[] args){
+        System.out.println(JDBCUtil.getConnection());
+    }
+}
